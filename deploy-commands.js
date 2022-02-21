3dba77cfc6943aae134eb1d6ce5fb3d724c216c3
Gesugao-san <gesugao.san@gmail.com>
2022-02-21 20:13:58 +0300

    Add command handling

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,40 +1,27 @@
-/* const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
 
-const commands = [{
-	name: 'ping',
-	description: 'Replies with Pong!'
-}];
+// node .\deploy-commands.js
 
-const rest = new REST({ version: '9' }).setToken('token');
-
-(async () => {
-	try {
-		console.log('Started refreshing application (/) commands.');
-
-		await rest.put(
-		Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
-		{ body: commands },
-		);
-
-		console.log('Successfully reloaded application (/) commands.');
-	} catch (error) {
-		console.error(error);
-	}
-})();
- */
-
+const fs = require('fs');
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const { clientId, guildId, token } = require('./config.json');
 
-const commands = [
+/* const commands = [
 	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
 	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
 	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
 ]
 	.map(command => command.toJSON());
+*/
+
+const commands = [];
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	commands.push(command.data.toJSON());
+}
 
 const rest = new REST({ version: '9' }).setToken(token);
 
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 
-// node index.js --trace-deprecation
+// 1: node index.js --trace-deprecation
+// 2: node .\deploy-commands.js
 // https://discordjs.guide/creating-your-bot/, https://github.com/FiredragonPlayz/discord.js-tutorials
 
 // Require the necessary discord.js classes
-const { Client,  Intents } = require("discord.js");
+const fs = require('fs');
+const { Client, Collection, Intents } = require('discord.js');
 const cfg = require("./config.json");
 
 // Create a new client instance
@@ -16,19 +18,34 @@ client.once('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
 });
 
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	// Set a new item in the Collection
+	// With the key as the command name and the value as the exported module
+	client.commands.set(command.data.name, command);
+}
+
 client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
+	const command = client.commands.get(interaction.commandName);
 
-	const { commandName } = interaction;
+	if (!command) return;
 
-	if (commandName === 'ping') {
-		await interaction.reply('Pong!');
-	} else if (commandName === 'server') {
-		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
-	} else if (commandName === 'user') {
-		await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 	}
 });
 
+process.on('SIGINT', function() {
+	console.log("Caught interrupt signal."); //"Medical examination: Killed by сaughted interrupt signal."
+	process.exit();
+});
+
 console.clear();
+console.log("Logging in...");
 client.login(cfg.token);
