13903eb365592a8ad5c9987ec97d480770afdae0
Gesugao-san <gesugao.san@gmail.com>
2022-02-22 14:42:24 +0300

    Add index_sequelize.js

---
+++ deploy-commands_sequelize.js
@@ -0,0 +1,24 @@
+
+// node ".\deploy-commands_sequelize.js" --trace-deprecation
+// https://discordjs.guide/sequelize/
+
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { clientId, guildId, token } = require('./config.json');
+
+const commands = [
+	new SlashCommandBuilder().setName('addtag').setDescription('Let\'s start with the ability to add a tag.'),
+	new SlashCommandBuilder().setName('tag').setDescription('Next, let\'s fetch the inserted tag.'),
+	new SlashCommandBuilder().setName('edittag').setDescription('Editing a tag.'),
+	new SlashCommandBuilder().setName('taginfo').setDescription('Display info on a specific tag.'),
+	new SlashCommandBuilder().setName('showtags').setDescription('Listing all tags.'),
+	new SlashCommandBuilder().setName('removetag').setDescription('Deleting a tag.'),
+]
+	.map(command => command.toJSON());
+
+const rest = new REST({ version: '9' }).setToken(token);
+
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+	.then(() => console.log('Successfully registered application commands.'))
+	.catch(console.error);
